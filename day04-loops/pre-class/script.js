1400a0234cb4cda9e90f466d910f9e5cbe4970ed
RafaellaValeeva <102030162+RafaellaValeeva@users.noreply.github.com>
2022-04-04 20:42:00 +0800

    Added Part 2

--- day04-loops/pre-class/script.js
+++ day04-loops/pre-class/script.js
@@ -1,23 +1,38 @@
 var followAlongMain = function (input) {
   // Attempt the Follow Along exercise from the Loops module below with followAlongMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var simpleLoopMain = function (input) {
   // Attempt the Simple Loop with Variations exercise from the Loops module below with simpleLoopMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+  var counter = 0;
+  while (counter < 6) {
+    myOutputValue = myOutputValue + "yes";
+    counter = counter + 1;
+  }
   return myOutputValue;
 };
 
 var loopWithinLoopMain = function (input) {
   // Attempt the Loop Within Loop exercise from the Loops module below with loopWithinLoopMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+  var rowCounter = 0;
+  while (rowCounter < input) {
+    var columnCounter = 0;
+    while (columnCounter < input) {
+      myOutputValue = myOutputValue + "x";
+      columnCounter = columnCounter + 1;
+    }
+    myOutputValue = myOutputValue + "<br>";
+    rowCounter = rowCounter + 1;
+  }
   return myOutputValue;
 };
 
 var infiniteLoopMain = function (input) {
   // Attempt the Infinite Loop exercise from the Loops module below with infiniteLoopMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
