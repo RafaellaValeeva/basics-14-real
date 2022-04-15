2bc317fd19a36a44f19c58fd53284e5e47b16665
RafaellaValeeva <102030162+RafaellaValeeva@users.noreply.github.com>
2022-04-15 18:41:32 +0800

    Finished Project 2

--- day04-loops/in-class/script.js
+++ day04-loops/in-class/script.js
@@ -1,53 +1,96 @@
 var emojiNumberCharactersMain = function (input) {
-  // Complete the Base: Emoji Drawing Number of Characters exercise below with emojiNumberCharactersMain as the main function.
-  var myOutputValue = 'hello world';
+  // // Complete the Base: Emoji Drawing Number of Characters exercise below with emojiNumberCharactersMain as the main function.
+  var myOutputValue = "";
+
+  for (var rowCounter = 0; rowCounter < input; rowCounter++) {
+    myOutputValue += "😀";
+    console.log(myOutputValue);
+  }
   return myOutputValue;
 };
 
 var emojiSquareMain = function (input) {
   // Complete the Base: Emoji Drawing Square exercise below with emojiSquareMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+
+  for (var rowCounter = 0; rowCounter < input; rowCounter++) {
+    for (var columnCounter = 0; columnCounter < input; columnCounter++) {
+      myOutputValue += "😀";
+    }
+    myOutputValue += "<br>";
+    console.log(myOutputValue);
+  }
   return myOutputValue;
 };
 
 var emojiTriangleMain = function (input) {
   // Complete the Comfortable: Emoji Drawing Triangle exercise below with emojiTriangleMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+  //var sideLength = Number(input);
+  for (var rowCounter = 0; rowCounter < input; rowCounter++) {
+    // Change the inner loop to only run outerCounter number of times
+    // This means that we only have as many 👍 on each line as the current row index.
+    // To ensure we always print the last emoji on each row, change the inner loop
+    // conditional to use <= instead of <.
+    for (var columnCounter = 0; columnCounter <= rowCounter; columnCounter++) {
+      myOutputValue += "😀";
+    }
+    myOutputValue += "<br>";
+    console.log(myOutputValue);
+  }
   return myOutputValue;
 };
 
 var emojiOutlineSquareMain = function (input) {
   // Complete the More Comfortable: Emoji Drawing Outline Square exercise below with emojiOutlineSquareMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+  for (var rowCounter = 0; rowCounter < input; rowCounter++) {
+    for (var columnCounter = 0; columnCounter < input; columnCounter++) {
+      // If current iteration represents a border element, draw ✊ instead.
+      if (
+        rowCounter == 0 ||
+        rowCounter == input - 1 ||
+        columnCounter == 0 ||
+        columnCounter == input - 1
+      ) {
+        myOutputValue += "✊";
+      } else {
+        // Add a 😀 to the current row
+        myOutputValue += "😀";
+      }
+    }
+    // Insert a line break to start a new row
+    myOutputValue += "<br>";
+  }
   return myOutputValue;
 };
 
 var emojiCenterSquareMain = function (input) {
   // Complete the More Comfortable: Emoji Drawing Center Square exercise below with emojiCenterSquareMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var multiDiceBaseMain = function (input) {
   // Complete the Base: Multi-Dice Game exercise below with multiDiceBaseMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var multiDiceMultiRoundMain = function (input) {
   // Complete the More Comfortable: Multi-Round Multi-Dice Game exercise below with multiDiceMultiRoundMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var multiDiceTwoPlayerMain = function (input) {
   // Complete the More Comfortable: Two Player Multi-Round Multi-Dice Game exercise below with multiDiceTwoPlayerMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var multiDiceMultiPlayerMain = function (input) {
   // Complete the More Comfortable: Multi-Player Multi-Round Multi-Dice Game exercise below with multiDiceMultiPlayerMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
--- day05-mad-libs/in-class/script.js
+++ day05-mad-libs/in-class/script.js
@@ -1,35 +1,64 @@
+var adjectives = [];
+
+var getRandomIndex = function (arrayLength) {
+  var randomIndex = Math.floor(Math.random() * arrayLength);
+  return randomIndex;
+};
+
 var madLibsAdjectivesMain = function (input) {
-  // Complete the Base: Mad Libs Adjectives exercise below with madLibsAdjectivesMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue;
+
+  if (input == "create") {
+    var randomIndex = getRandomIndex(adjectives.length);
+    var randomAdjective = adjectives[randomIndex];
+    var madLib = `You will never believe what I saw today! It was a ${randomAdjective} dog carrying a stick.`;
+    myOutputValue = madLib;
+  } else {
+    var found = false; // default : NOT FOUND
+
+    for (var index = 0; index < adjectives.length; index += 1) {
+      // check if input and adj #index is same - if yes then true
+      var currentWord = adjectives[index];
+      if (input === currentWord) {
+        found = true; // if we find it, FOUND
+      }
+    }
+    if (!found) {
+      adjectives.push(input); // only push into array if NOT FOUND
+    }
+    myOutputValue = adjectives;
+  }
+
+  //if (!found) =
   return myOutputValue;
 };
 
 var madLibsInputCreateMain = function (input) {
   // Complete the Comfortable: Input and Create Mode exercise below with madLibsInputCreateMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var madLibsMultipleWordsMain = function (input) {
   // Complete the Comfortable: Input Multiple Words exercise below with madLibsMultipleWordsMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var madLibsMultipleTypesMain = function (input) {
   // Complete the More Comfortable: Mad Libs Multiple Word Types exercise below with madLibsMultipleTypesMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var madLibsPopularMain = function (input) {
   // Complete the More Comfortable: Popular Mad Libs exercise below with madLibsPopularMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var madLibsSetsMain = function (input) {
   // Complete the More Comfortable: Sets of Mad Libs exercise below with madLibsSetsMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
